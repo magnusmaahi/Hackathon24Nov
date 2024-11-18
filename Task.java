f8086738de6184b76548234035d4902e661f5d8c
mahinvitha <pmahinvitha@gmail.com>
2024-11-18 22:29:16 +0000

    Implemented task

---
+++ Task.java
@@ -0,0 +1,22 @@
+public class Task{
+
+	private String description;
+	private boolean isCompleted;
+	private String priority;
+	private int timeNeeded;
+	
+	public Task(String d, String p, int t){
+		description = d;
+		isCompleted = false;
+		priority = p;
+		timeNeeded = t;
+	}
+	
+	public void setCompleted(boolean status){
+		this.isCompleted = status;
+	}
+	
+	public String toString(){
+		return String.format("Description.          | Status.  | Priority.    | Time needed\n%-35s| %-9s| %-14s| %s mins", description, isCompleted, priority, timeNeeded);
+	}
+}
