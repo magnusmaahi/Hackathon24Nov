e07c6c6b49d738078c591dfd33dc9140f92c178d
aaCoder8 <aditiadyausa11@gmail.com>
2024-11-20 18:28:50 -0600

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,22 +1,16 @@
 public class Task{
-
 	private String description;
-	private boolean isCompleted;
-	private String priority;
+	private int priority;
 	private int timeNeeded;
 	
-	public Task(String d, String p, int t){
+	public Task(String d, int p, int t){
 		description = d;
 		isCompleted = false;
 		priority = p;
 		timeNeeded = t;
 	}
 	
-	public void setCompleted(boolean status){
-		this.isCompleted = status;
-	}
-	
 	public String toString(){
-		return String.format("          Description.          |  Status.  |  Priority.  | Time needed\n%-35s| %-9s| %-14s| %s mins", description, isCompleted, priority, timeNeeded);
+		return String.format("          Description.          |  Priority.  | Time needed\n%-35s| %-9s| %-14d| %d mins", description, priority, timeNeeded);
 	}
 }
