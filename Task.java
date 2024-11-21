471e1e1ddb1c9aa0a9a6f0850f8f6842b59580e3
aaCoder8 <aditiadyausa11@gmail.com>
2024-11-20 19:57:05 -0600

    Update Task.java
    commented out og task file and added new one

--- Task.java
+++ Task.java
@@ -1,4 +1,4 @@
-public class Task{
+/* public class Task{
 	private String description;
 	private int priority;
 	private int timeNeeded;
@@ -32,6 +32,49 @@ public class Task{
 	}
 	
 	public boolean getTime(){
+		return timeNeeded;
+	}
+  
+	public String toString(){
+		return String.format("          Description.          |  Priority.  | Time needed\n%-35s| %-9s| %-14d| %d mins", description, priority, timeNeeded);
+	}
+} */
+ public class Task{
+	private String description;
+	private boolean isCompleted;
+	private int priority;
+	private int timeNeeded;
+	
+	public Task(String d, int p, int t){
+		description = d;
+		isCompleted = false;
+		priority = p;
+		timeNeeded = t;
+	}
+	
+
+	//set method for the status
+	public void setCompleted(boolean status){
+		this.isCompleted = status;
+	}
+	//getter methods
+	public boolean getStatus(){
+		return isCompleted;
+	}
+	
+	public int getPriority(){
+		/* if (priority == 1) {
+			return "Right NOW!";
+		}
+		else if (priority == 2) {
+			return "ASAP";
+		} else {
+			return "End of day";
+		} */
+		return this.priority;
+	}
+	
+	public int getTime(){
 		return timeNeeded;
 	}
   
