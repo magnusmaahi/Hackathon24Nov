74fbea7df07bb6b7ce4e318aa1f33c8ce5eab01a
mahinvitha <pmahinvitha@gmail.com>
2024-11-20 20:21:54 -0600

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,44 +1,3 @@
-/* public class Task{
-	private String description;
-	private int priority;
-	private int timeNeeded;
-	
-	public Task(String d, int p, int t){
-		description = d;
-		isCompleted = false;
-		priority = p;
-		timeNeeded = t;
-	}
-	
-
-	//set method for the status
-	public void setCompleted(boolean status){
-		this.isCompleted = status;
-	}
-	//getter methods
-	public boolean getStatus(){
-		return isCompleted;
-	}
-	
-	public Strring getPriority(){
-		if (priority == 1) {
-			return "Right NOW!";
-		}
-		else if (priority == 2) {
-			return "ASAP";
-		} else {
-			return "End of day";
-		}
-	}
-	
-	public boolean getTime(){
-		return timeNeeded;
-	}
-  
-	public String toString(){
-		return String.format("          Description.          |  Priority.  | Time needed\n%-35s| %-9s| %-14d| %d mins", description, priority, timeNeeded);
-	}
-} */
  public class Task{
 	private String description;
 	private boolean isCompleted;
@@ -79,6 +38,6 @@
 	}
   
 	public String toString(){
-		return String.format("          Description.          |  Priority.  | Time needed\n%-35s| %-9s| %-14d| %d mins", description, priority, timeNeeded);
+		return String.format("          Description.          |  Priority.  | Time needed\n%-35s| %-9s| %d mins", description, priority, timeNeeded);
 	}
 }
