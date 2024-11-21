b84094dfa8b9d5091174807bb3203ade1d9752e9
mahinvitha <pmahinvitha@gmail.com>
2024-11-20 20:17:22 -0600

    Update Main.java
    working main

--- Main.java
+++ Main.java
@@ -1,101 +1,4 @@
-/* import java.util.*;
-
-import static java.lang.System.out;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<>();
-        out.println("TO-DO List");
-        out.print("Create your list");
-        int opt, pN, t;
-        String tN;
-
-        do {
-            //main menu
-            out.println("1. Create a new task\n2. Print my task list(s)\n3. Exit");
-            opt = s.nextInt();
-        } while (c) {
-            //Option 1
-            if (opt == 1) {
-                out.print("Task name? (Character limit 40): ");
-                tN = s.nextLine();
-                
-                while (tN.length() > 40) {
-                    out.println("Oops!! You exceeded the character limit. Shorten PLS");
-                    out.print("Task name? (Character limit 40): ");
-                    tN = s.nextLine();
-                }
-
-                //priority
-                out.print("\nPick your priority (1- RIGHT NOW! 2 - ASAP 3- End of Day): ");
-                pN = s.nextInt();
-                    
-                //time needed
-                out.print("\nEstimate how long this task might take you (in hours and minutes): ");//we can offer a time using our clock lab we did in cs3
-                int h = s.nextInt();
-                int m = s.nextInt();
-                t = time(h, m); 
-                    
-                tasks.add(new Task(tN, pN, t));
-            }
-        
-            //Option 2
-            else if (opt == 2){
-                if (tasks.size() == 0) {
-                    out.println("GUESS WHAT!! you got no tasks");
-                }
-                else {
-                    printTasks(tasks);
-                }
-            }
-
-            //Option 3
-            else {
-                c = false;
-                break;
-            }
-        }
-    }
-
-    public static int time(int h, int m) {
-        return m + (h * 60);
-    }
-
-    public static void printTasks(ArrayList<Task> list) {
-    HashMap<Integer, ArrayList<Task>> map = new HashMap<>();
-    int index = 0;
-
-    // Populate the HashMap with tasks grouped by priority
-    while (index < list.size()) {
-        int priority = list.get(index).getPriority();
-        Task t = list.get(index);
-
-        if (map.containsKey(priority)) {
-            map.get(priority).add(t);
-        } else {
-            ArrayList<Task> taskList = new ArrayList<>();
-            taskList.add(t);
-            map.put(priority, taskList);
-        }
-        index++;
-    } // End of while loop
-
-    // Print tasks grouped by priority
-    for (int i = 1; i <= 4; i++) {
-        ArrayList<Task> taskList = map.get(i); // Get the list for priority i
-
-        if (taskList != null) { // Check to avoid NullPointerException
-            for (Task t : taskList) {
-                System.out.println(t.toString());
-            }
-        }
-    } // End of printing tasks
-} // End of method  
-} */
-
 import java.util.*;
-
 import static java.lang.System.out;
 
 public class Main {
@@ -108,72 +11,70 @@ public class Main {
         int opt, pN, t;
         String tN;
 
-        
-        do{
+        do {
+            // Main menu
+            out.println("1. Create a new task\n2. Print my task list(s)\n3. Complete task\n4. Exit");
+            opt = s.nextInt();
+            s.nextLine(); // Consume the leftover newline
             
-        //main menu
-        out.println("1. Create a new task\n2. Print my task list(s)\n3. Complete task\n4. Exit");
-        opt = s.nextInt();
-        s.nextLine();
-            //Option 1
+            // Option 1: Create a new task
             if (opt == 1) {
                 out.print("Task name? (Character limit 40): ");
                 tN = s.nextLine();
-                
                 while (tN.length() > 40) {
                     out.println("Oops!! You exceeded the character limit. Shorten PLS");
                     out.print("Task name? (Character limit 40): ");
                     tN = s.nextLine();
                 }
 
-                //priority
-                out.print("\nPick your priority (1- RIGHT NOW! 2 - ASAP 3- End of Day): ");
+                // Priority
+                out.print("\nPick your priority (1- RIGHT NOW! 2 - ASAP 3 - End of Day): ");
                 pN = s.nextInt();
-                    
-                //time needed
-                out.print("\nEstimate how long this task might take you (in hours and minutes): ");//we can offer a time using our clock lab we did in cs3
+
+                // Time needed
+                out.print("\nEstimate how long this task might take you (in hours and minutes): ");
                 int h = s.nextInt();
                 int m = s.nextInt();
-                t = time(h, m); 
-                    
+                t = time(h, m);
+
+                // Add the task to the to-do list
                 toDo.add(new Task(tN, pN, t));
-                
-                /* main menu
-                out.println("1. Create a new task\n2. Print my task list(s)\n3. Complete task\n4. Exit");
-                opt = s.nextInt();
-                s.nextLine(); */
             }
-        
-            //Option 2
-            else if (opt == 2){
-                if (toDo.size() == 0) {
+
+            // Option 2: Print task lists
+            else if (opt == 2) {
+                if (toDo.isEmpty()) {
                     out.println("GUESS WHAT!! you got no tasks");
-                }
-                else {
+                } else {
+                    out.println("TO-DO List:");
                     printTasks(toDo);
                 }
+                if (!done.isEmpty()) {
+                    out.println("\nCompleted Tasks:");
+                    printTasks(done);
+                }
+            }
 
-                //main menu
-                out.println("1. Create a new task\n2. Print my task list(s)\n3. Complete task\n4. Exit");
-                opt = s.nextInt();
-                s.nextLine();
-            }
-            
+            // Option 3: Complete a task
             else if (opt == 3) {
-            	printTasks(toDo);
-            	
-                //main menu
-                out.println("1. Create a new task\n2. Print my task list(s)\n3. Complete task\n4. Exit");
-                opt = s.nextInt();
-                s.nextLine();
+                if (toDo.isEmpty()) {
+                    out.println("No tasks to complete!");
+                } else {
+                    out.println("TO-DO List:");
+                    printTasks(toDo);
+                    out.println("Enter the number of the task you completed: ");
+                    int taskNum = s.nextInt();
+                    if (taskNum > 0 && taskNum <= toDo.size()) {
+                        done.add(toDo.remove(taskNum - 1)); // Move task to "done"
+                        out.println("Task marked as completed!");
+                    } else {
+                        out.println("Invalid task number.");
+                    }
+                }
             }
-            
-            //main menu
-            out.println("1. Create a new task\n2. Print my task list(s)\n3. Complete task\n4. Exit");
-            opt = s.nextInt();
-            s.nextLine();
-        }while (opt != 4);
-        
+
+        } while (opt != 4);
+
         out.println("Have a great day!");
     }
 
@@ -182,12 +83,6 @@ public class Main {
     }
 
     public static void printTasks(ArrayList<Task> list) {
-        /* for (int i = 1; i < list.size() + 1; i++) {
-        	out.println(i + ". " + list.get(i - 1));
-        }
-        ArrayList<Task> sorted = new ArrayList<>(); */
-    	
-
         HashMap<Integer, ArrayList<Task>> map = new HashMap<>();
         int index = 0;
 
@@ -207,7 +102,7 @@ public class Main {
         } // End of while loop
 
         // Print tasks grouped by priority
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= 3; i++) { // Only priorities 1-3 are supported
             ArrayList<Task> taskList = map.get(i); // Get the list for priority i
 
             if (taskList != null) { // Check to avoid NullPointerException
@@ -216,5 +111,5 @@ public class Main {
                 }
             }
         } // End of printing tasks
-    }  
+    }
 }
